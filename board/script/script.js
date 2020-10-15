ae263e334e73ceed658c4fda3bfbf48e2f4e392c
finn <kwonque@gmail.com>
2020-10-15 16:21:21 +0900

    board

---
+++ board/script/script.js
@@ -0,0 +1,28 @@
+
+$(window).load(function(){
+    var $grid = $('.grid');
+    $grid.masonry({
+        itemSelector:'.grid-item'
+    })
+
+    /*로그인*/
+    $("#modal-open").click(function(){
+        $("#modal").attr("style","display:block");
+       
+    });
+    $("#modal-close").click(function(){
+        $("#modal").attr("style","display:none");
+    });
+
+    /*회원가입 */
+    $("#modal-open2").click(function(){
+        $("#modal2").attr("style","display:block");
+       
+    });
+    $("#modal-close2").click(function(){
+        $("#modal2").attr("style","display:none");
+    });
+})
+
+
+
