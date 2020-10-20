ce2d5c763680140a25d36360c458296484bc0315
권민지 <alswl11317@gmail.com>
2020-10-20 15:23:22 +0900

    calendar js css

--- Calendar/script.js
+++ Calendar/script.js
@@ -1,14 +1,23 @@
 let today = new Date();
+
 let currentMonth = today.getMonth()+1;
 let currentYear = today.getFullYear();
 
-
+//오늘 년월
 const todayYear = today.getFullYear();
 const todayMonth =today.getMonth();
 // let btnBackY = document.getElementById("btnBackY");
 // let btnForwardY = document.getElementById("btnForwardY");
 // let btnBackM = document.getElementById("btnBackM");
 // let btnForwardM = document.getElementById("btnForwardM");
+
+HTMLElement.prototype.css = function(object){
+    const element = this.style;
+    for(let k in object){
+       element[k] = object[k];
+    }
+    return this;
+ }
 
 window.onload = function(){
     mkCalendar(currentYear, currentMonth);
@@ -61,55 +70,44 @@ function mkCalendar(year, month){
     }
 
     let row = null;
-    row = tbCalendar.insertRow();
+    row = tbCalendar.insertRow(); //개행
 
-    var cnt = 0;
-    var not = 0;
+    var cnt = 0; //날짜
+    var not = 0; //저번 달
+    //월 시작 공백을 채우는 부분
     for(i=0;i<firstDate.getDay();i++){
         cell = row.insertCell();
         cell.innerHTML=plastDate.getDate()-firstDate.getDay()+1+i;
         cnt +=1;
         not = cnt;
-        cell.style.opacity="0.7";
-        if(cnt %7 ==1){
-            cell.style.color="#F34054";
-        }else if(cnt %7 == 0){
-            cell.style.color="#4054F3";
+        cell.css({opacity:"0.7"});
+        if(cnt %7 == 0) // 개행
             row = tbCalendar.insertRow();
-        }
     }
-    
+    //이번달
     for(i=1;i<=lastDate.getDate(); i++){
-        cell = row.insertCell();
-        cell.innerHTML = i;
-        cnt +=1;
+
+        cell = row.insertCell(); //cell 추가
+        cell.innerHTML = i; //cell에 숫자(날짜) 추가
+        cnt +=1; //숫자(날짜) count+1
+
+        //오늘 날짜에 .today 주어서 css추가
         if(year == todayYear && month == todayMonth+1){
             if(cnt-not == today.getDate()){
-                cell.style.fontWeight="bold";
-                cell.style.backgroundColor="#EDEDED";
-                cell.style.borderRadius="50%";
+                cell.classList.add("today");
             }
         }
-        
-        if(cnt %7 ==1){
-            cell.style.color="#F34054";
-        }else if(cnt %7 == 0){
-            cell.style.color="#4054F3";
-            row = tbCalendar.insertRow();
-        }
-    }
 
-    for (i = cnt, j = 1; i < 42; i++,j++) {
+        if(cnt %7 == 0)// 개행
+            row = tbCalendar.insertRow();
+    }
+    //cell을 7*6으로 만들어서 높이 고정
+    for (i = cnt, j = 1; i < 42; i++,j++) { //j는 다음 달
         cell = row.insertCell();
         cell.innerHTML = j;
-        cell.style.opacity="0.7";
+        cell.css({opacity:"0.7"});
         cnt += 1;
-        if (cnt % 7 == 1) {
-            cell.style.color = "#F34054";
-        } else if (cnt % 7 == 0) {
-            cell.style.color = "#4054F3";
+        if (cnt % 7 == 0)// 개행
             row = tbCalendar.insertRow();
-
-        }
     }   
 }
