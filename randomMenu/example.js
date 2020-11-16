7d5d4bf76bdf2bc84669727790313c7b2bd0a867
finn <kwonque@gmail.com>
2020-11-16 11:15:48 +0900

    create no meat button

--- randomMenu/example.js
+++ randomMenu/example.js
@@ -2,12 +2,28 @@ const menuList = ["배할머니", "왕돈까스&왕냉면", "엄마손칼국수"
  "편의점*^^*", "쌀국수", "투가리", "청년다방", "서브웨이",
   "육쌈냉면", "6000원 밥집", "육쌈옆 밥집"];
 
-window.onload = function () {
-    randMenu();
-}
+const noMenuList = ["배할머니", "왕돈까스&왕냉면", "엄마손칼국수", "투가리",
+"서브웨이"];
 
-function randMenu() {
-    const $menuText = document.getElementById("text");
-    const i = Math.floor(Math.random()*menuList.length);
-    $menuText.textContent = menuList[i];
+
+window.onload = function () {
+    let isNoMeat = false;
+
+    const NoButton = document.getElementById("btnNo");
+    const StartButton = document.getElementById("btnStart");
+
+    NoButton.addEventListener('click', 
+    function(){isNoMeat=true; randMenu(isNoMeat);});
+    StartButton.addEventListener('click', 
+    function(){isNoMeat=false; randMenu(isNoMeat);});
+}
+function randMenu(type) {
+    const $menuText = document.getElementById("Text");
+    if(type){
+        const i = Math.floor(Math.random()*noMenuList.length);
+        $menuText.textContent = noMenuList[i];
+    }else{
+        const i = Math.floor(Math.random()*menuList.length);
+        $menuText.textContent = menuList[i];
+    }
 }
